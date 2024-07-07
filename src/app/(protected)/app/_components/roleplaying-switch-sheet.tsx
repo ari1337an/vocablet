3a9731dcbeb01c5389cc0a8868431cfcfd6e49cf
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2024-07-07 17:51:12 +0600

    fix: hydration error

--- src/app/(protected)/app/_components/roleplaying-switch-sheet.tsx
+++ src/app/(protected)/app/_components/roleplaying-switch-sheet.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   Sheet,
   SheetContent,
@@ -8,28 +8,7 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-  SheetFooter,
 } from "@/app/_components/ui/sheet";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/app/_components/ui/form";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/app/_components/ui/command";
-import { cn } from "@/app/_lib/utils";
-import { CheckIcon } from "@radix-ui/react-icons";
-import { ScrollArea } from "@/app/_components/ui/scroll-area";
-import { z } from "zod";
 import {
   Tabs,
   TabsContent,
@@ -40,11 +19,7 @@ import { Button } from "@/app/_components/ui/button";
 import useAppStore from "../../_store/useAppStore";
 import { Switch } from "@/app/_components/ui/switch";
 import { Label } from "@/app/_components/ui/label";
-import { Input } from "@/app/_components/ui/input";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import toast from "react-hot-toast";
-import { Textarea } from "@/app/_components/ui/textarea";
 import { RoleplayTab } from "./roleplay-tab";
 
 interface Roleplay {
@@ -62,7 +37,6 @@ export function RoleplayingSwitchSheet({
   conversationOngoing: boolean;
 }) {
   const { roleplayMode, setRoleplayMode } = useAppStore();
-  const [isMounted, setIsMounted] = useState(false);
   const [open, setOpen] = useState(false);
   const [selectedRoleplay, setSelectedRoleplay] = useState<Roleplay | null>(
     null
@@ -93,27 +67,21 @@ export function RoleplayingSwitchSheet({
     }
   };
 
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
   return (
     <Sheet open={open} onOpenChange={setOpen}>
-      <SheetTrigger>
-        {isMounted ? (
+      <SheetTrigger asChild>
+        <>
           <Switch
             checked={roleplayMode.agent === "roleplay"}
             id="roleplay-mode"
           />
-        ) : (
-          <div></div>
-        )}
-        <Label>Roleplay Mode</Label>
+          <Label htmlFor="roleplay-mode">Roleplay Mode</Label>
+        </>
       </SheetTrigger>
       <SheetContent className="h-full overflow-y-auto">
         <SheetHeader>
           <SheetTitle>Roleplay Mode</SheetTitle>
-          {(selectedRoleplay  || activeTab == "general")&& (
+          {(selectedRoleplay || activeTab == "general") && (
             <Button onClick={() => handleSetButton()}>Set</Button>
           )}
           <SheetDescription>{`Select a roleplaying template or create new`}</SheetDescription>
