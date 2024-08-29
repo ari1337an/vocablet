7c1914047b4adccf0514740b9bbba2a9e305ec81
Md Sahadul Hasan Arian <alphawolfventures@gmail.com>
2024-08-29 15:56:04 +0600

    chore: SEO json-ld

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,47 @@ import Hero from "@/app/_components/Hero";
 import Pricing from "@/app/_components/Pricing";
 import Video from "@/app/_components/Video";
 import { Metadata } from "next";
-import "./react-modal.css"
+import "./react-modal.css";
 
 export const metadata: Metadata = {
   title: "Vocablet | AI-Powered Vocabulary Learning Platform",
   description:
-    "Boost your vocabulary effortlessly with Vocablet. Our AI-powered platform helps you learn new words, practice with flashcards, and engage in conversations with your favorite characters. Join now to enhance your language skills quickly and enjoyably.",
+    "Vocablet is a personalized vocabulary learning app that helps users expand their vocabulary through interactive role-playing and flashcards.",
+};
+
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "@id": "https://www.wikidata.org/wiki/Q130047855", // Replace or remove if not applicable
+  name: "Vocablet",
+  url: "https://vocablet.io/",
+  logo: "https://vocablet.io/favicon.ico",
+  founder: [
+    {
+      "@type": "Person",
+      name: "Arian S. Hasan", // Replace with the actual founder's name
+    },
+    {
+      "@type": "Person",
+      name: "Faisal Ahmed Sifat", // Replace with the actual founder's name
+    },
+  ],
+  description:
+    "Vocablet is a personalized vocabulary learning app that helps users expand their vocabulary through interactive role-playing and flashcards.", // Assuming you have a dynamic description
+  contactPoint: {
+    "@type": "ContactPoint",
+    telephone: "+1-302-4519439", // Replace with actual contact number
+    contactType: "Customer Service",
+  },
 };
 
 export default function Home() {
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <Header />
       <ScrollUp />
       <Hero />
